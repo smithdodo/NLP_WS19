1fad84652f3c88e40681fbab85ba40a2da957856
Hengwen Zhang <h.w.zhangvic@gmail.com>
2019-11-06 23:25:16 +0100

    Implement classifier using pandas

---
+++ group01/03/classifier_pd.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import numpy as np
+from collections import Counter
+
+
+def get_voc(size, path):
+    with open(path, 'r') as f:
+        all_vocs = [w.strip() for w in f.read().split('\n')]
+        limit = size if size < len(all_vocs) else len(all_vocs)
+        voc = {v.strip():0 for v in all_vocs[:limit]}
+        voc['<UNK>'] = 0
+    return voc
+
+
+def read_training_data(text_path, voc):
+    with open(text_path, 'r') as f:
+        rows = []
+        for line in f.readlines():
+            row = voc.copy()
+            elements = [el for el in line.strip().split(' ') if el]
+            row['class_name'] = elements[1]
+            for word, count in zip(elements[2::2], elements[3::2]):
+                if word in row:
+                    row[word] = int(count)
+                else:
+                    row['<UNK>'] += 1
+            rows.append(row)
+
+    df = pd.DataFrame(rows)
+    # put class_name on first col
+    cols = df.columns.tolist()
+    cols.remove('class_name')
+    cols.insert(0, 'class_name')
+    df = df[cols]
+    return df
+
+
+def train(df):
+    """
+    Return p(w|c) and p(c)
+    """
+    # p(c)
+    pc = {k: v/len(df) for k,v in Counter(df['class_name']).items()}
+
+    # convert word counts to relative freq
+    df['length'] = df.iloc[:, 1:-1].sum(axis=1)
+    df = df.groupby('class_name').sum()
+    # p(w|c)
+    df= df.iloc[:, 0:-1].div(df['length'], axis=0)
+
+    print(df.head())
+    return df, pc
+
+
+def predict(word_counts, p_w_c, prior):
+    """
+    return argmax_c{p(c)p(w|c)}
+    """
+    best_class, best_val = "", 0
+    for class_, row in p_w_c.iterrows():
+        val = prior[class_] * np.prod([row[word] ** count for word, count in word_counts.items()])
+        if val > best_val:
+            best_class = class_
+            best_val = val
+    
+    print(best_class, best_val)
+    return best_class
+
+
+def evaluate():
+    voc = get_voc(400, '20news/20news.voc')
+    p_w_c, prior = train(read_training_data('20news/20news.tr', voc))
+    test = read_training_data('20news/20news.te', voc)
+
+    results = []
+    for idx, row in test.iterrows():
+        actual = row['class_name']
+        pred = predict(row[1:], p_w_c, prior)
+        results.append((actual, pred))
+        # print(f'actual: {actual}, pred: {pred}')
+    
+    print('err:', len([1 for i in results if i[0] != i[1]]) / len(results))
+
+evaluate()
