69f50b61dada98968f3a92d4efbe272e453923e6
Hengwen Zhang <h.w.zhangvic@gmail.com>
2019-11-07 17:17:42 +0100

    Massive performance improvement

--- group01/03/classifier_pd.py
+++ group01/03/classifier_pd.py
@@ -7,7 +7,7 @@ def get_voc(size, path):
     with open(path, 'r') as f:
         all_vocs = [w.strip() for w in f.read().split('\n')]
         limit = size if size < len(all_vocs) else len(all_vocs)
-        voc = {v.strip():0 for v in all_vocs[:limit]}
+        voc = {v.strip(): 0 for v in all_vocs[:limit]}
         voc['<UNK>'] = 0
     return voc
 
@@ -21,7 +21,7 @@ def read_training_data(text_path, voc):
             row['class_name'] = elements[1]
             for word, count in zip(elements[2::2], elements[3::2]):
                 if word in row:
-                    row[word] = int(count)
+                    row[word] += int(count)
                 else:
                     row['<UNK>'] += 1
             rows.append(row)
@@ -37,48 +37,46 @@ def read_training_data(text_path, voc):
 
 def train(df):
     """
-    Return p(w|c) and p(c)
+    Return log likelyhood p(w|c) and prior p(c)
     """
     # p(c)
-    pc = {k: v/len(df) for k,v in Counter(df['class_name']).items()}
+    pc = pd.DataFrame([{k: v/len(df) for k, v in Counter(df['class_name']).items()}])
 
     # convert word counts to relative freq
     df['length'] = df.iloc[:, 1:-1].sum(axis=1)
     df = df.groupby('class_name').sum()
-    # p(w|c)
-    df= df.iloc[:, 0:-1].div(df['length'], axis=0)
+    # log p(w|c)
+    df = np.log10(df.iloc[:, 0:-1].div(df['length'], axis=0))
 
-    print(df.head())
     return df, pc
 
 
 def predict(word_counts, p_w_c, prior):
     """
-    return argmax_c{p(c)p(w|c)}
+    return argmax_c{p(c)mul_w{p(w|c)^N_w}}
     """
-    best_class, best_val = "", 0
-    for class_, row in p_w_c.iterrows():
-        val = prior[class_] * np.prod([row[word] ** count for word, count in word_counts.items()])
-        if val > best_val:
-            best_class = class_
-            best_val = val
-    
-    print(best_class, best_val)
+    res = (p_w_c.as_matrix() * word_counts).sum(axis=1) * prior.values
+    best_class = prior.columns[res.argmax()]
+    print(best_class)
     return best_class
 
 
 def evaluate():
-    voc = get_voc(400, '20news/20news.voc')
-    p_w_c, prior = train(read_training_data('20news/20news.tr', voc))
-    test = read_training_data('20news/20news.te', voc)
+    voc = get_voc(1000, '20news/20news.voc')
+    # p_w_c, prior = train(read_training_data('20news/20news.tr', voc))
+    # test = read_training_data('20news/20news.te', voc)
+    p_w_c, prior = train(read_training_data('spam/spam.tr', voc))
+    test = read_training_data('spam/spam.te', voc)
 
     results = []
     for idx, row in test.iterrows():
         actual = row['class_name']
-        pred = predict(row[1:], p_w_c, prior)
+        pred = predict(row[1:].values, p_w_c, prior)
         results.append((actual, pred))
         # print(f'actual: {actual}, pred: {pred}')
-    
+
     print('err:', len([1 for i in results if i[0] != i[1]]) / len(results))
 
-evaluate()
+
+if __name__ == '__main__':
+    evaluate()
