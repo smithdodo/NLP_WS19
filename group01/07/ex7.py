d84149fb1a966e4bbff98b6f5a8d82eb3069aaa8
Hengwen Zhang <h.w.zhangvic@gmail.com>
2019-12-02 22:07:04 +0100

    Bugfix

--- group01/07/ex7.py
+++ group01/07/ex7.py
@@ -13,44 +13,39 @@ def train(corpus, taggings):
     vocabulary = set()
     tags = set()
 
-    with open(corpus) as f_corpus:
-        with open(taggings) as f_pos:
-            seen = deque(['<S>'], maxlen=2)
+    f = read_files([corpus, taggings])
+    seen = deque(['<S>'], maxlen=2)
 
-            cnt = 0
+    cnt = 0
 
-            while True:
-                try:
-                    sentance = next(f_corpus)
-                    tagging = next(f_pos)
-                except StopIteration:
-                    break
+    while True:
+        try:
+            sentance, tagging = next(f)
+        except Exception:
+            break
 
-                for word, tag in zip(_break_line(sentance), _break_line(tagging)):
-                    if not re.match(r'\w+', tag):
-                        continue
+        for word, tag in zip(_break_line(sentance), _break_line(tagging)):
+            if not re.match(r'\w+', tag):
+                continue
 
-                    seen.append(tag)
+            seen.append(tag)
 
-                    # update models
-                    bigram_pos[tuple(seen)] += 1
-                    p_w_given_g[(word, tag)] += 1
-                    vocabulary.add(word)
-                    tags.add(tag)
+            # update models
+            bigram_pos[tuple(seen)] += 1
+            p_w_given_g[(word, tag)] += 1
+            vocabulary.add(word)
+            tags.add(tag)
 
-                cnt += 1
-                if cnt >= 10000:
-                    break
+        cnt += 1
+        # if cnt >= 1000:
+        #     break
     
-
     # add-1 smoothing for p(g_i|g_{i-1})
     for key in product(tags, repeat=2):
         bigram_pos[key] += 1
 
     # add-1 smoothing for p(w|g)
     for w in vocabulary:
-        # if w == 'Vinken':
-        #     import ipdb; ipdb.set_trace()
         for tag in tags:
             p_w_given_g[(w, tag)] += 1
 
@@ -70,8 +65,8 @@ def train(corpus, taggings):
     p_w_given_g = p_w_given_g / p_w_given_g.groupby('g').sum()
 
 
-    print(bigram_pos.head())
-    print(p_w_given_g.head())
+    bigram_pos = {k: v['count'] for k, v in bigram_pos.to_dict('index').items()}
+    p_w_given_g = {k: v['count'] for k, v in p_w_given_g.to_dict('index').items()}
     return bigram_pos, p_w_given_g, list(tags), list(vocabulary)
 
 
@@ -83,67 +78,115 @@ def _break_line(line):
 
 def tag(sentance, bigram_pos, p_w_given_g, tags, vocabulary):
     words = _break_line(sentance)
-
     results = []
-
-    print(tags)
 
     # stores all O(n, g)
     probs = []
-    probs.append(
-        {g: {'prob':p_w_given_g.loc[words[0], g]['count'], 'g_prev': None} for g in tags})
+    if words[0] in vocabulary:
+        probs.append(
+            {g: {'prob': p_w_given_g[(words[0], g)], 'g_prev': None} for g in tags})
+    else:
+        probs.append(
+            {g: {'prob': 1/len(vocabulary), 'g_prev': None} for g in tags})
 
-    for w in words[1:]:
+    for w in [w for w in words[1:] if is_token(w)]:
         result = {}
-        if w not in vocabulary:
-            continue
 
         for g in tags:
-
             # {O(n-1, g')*g(g|g') | g'}
-            # try:
             prev_probs = np.array([
-                probs[-1][g_prev]['prob'] * bigram_pos.loc[g_prev, g]['count'] for g_prev in tags])
-            # except KeyError as e:
-            #     import ipdb; ipdb.set_trace()
-            #     print(e)
-
+                probs[-1][g_prev]['prob'] * bigram_pos[(g_prev, g)] for g_prev in tags])
             choice = np.argmax(prev_probs)
-            prob = p_w_given_g.loc[w, g] * prev_probs[choice]
 
+            try:
+                member_ship_prob = p_w_given_g[(w, g)]
+            except KeyError:
+                # handling OOV
+                member_ship_prob = 1 / len(vocabulary)
+
+            prob = member_ship_prob * prev_probs[choice]
             result[g] = {'prob': prob, 'g_prev': tags[choice]}
-
 
         probs.append(result)
 
-
+    # select best tagging
     tagging = []
 
     idx = np.argmax(np.array([d['prob'] for d in probs[-1].values()]))
     tagging.append(tags[idx])
     last_choice = probs[-1][tagging[0]]['g_prev']
-    tagging.insert(0, last_choice)
+    tagging.append(last_choice)
 
     for prob in reversed(probs[1:-1]):
         last_choice = prob[last_choice]['g_prev']
-        tagging.insert(0, last_choice)
+        tagging.append(last_choice)
     
-    tagging = list(reversed(tagging))
-    for w in words:
-        if re.match(r'\W+', w): 
-            print(w)
-            continue
-        print(f'{w:10s} {tagging.pop()}')
-    # print(tagging)
-    # print(words)
+    tagging.reverse()
+
+    result = []
+    for i, w in enumerate(words):
+        if not is_token(w):
+            # print(w)
+            result.append(w)
+        else:
+            result.append(tagging.pop(0))
+
+        print(f'{w:10s} -> {result[-1]}')
+
+    return result
+
+
+def is_token(w):
+    return re.match(r'^\W+$', w) is None
+
+
+def read_files(paths):
+    files = [open(path, 'r') for path in paths]
+    try:
+        while True:
+            yield [next(f) for f in files]
+    except StopIteration:
+        for f in files:
+            f.close()
+        raise
+
+
+def score(bigram, p_w_given_g, tags, vocabulary):
+    corpus = 'wsj/wsj.text.test'
+    pos = 'wsj/wsj.pos.test'
+    f = read_files([corpus, pos])
+
+    result = []
+
+    cnt = 0
+    while True:
+
+        try:
+            X, y = next(f)
+            y = y.strip().split(' ')
+            tagging = tag(X, bigram, p_w_given_g, tags, vocabulary)
+            result += list(zip(y, tagging))
+        except Exception:
+            break
+
+        cnt += 1
+        # if cnt > 10:
+        #     break
+    
+    print(
+        'Accuracy on test data:',
+         sum([1 if (y[0] == y[1] or re.match('^\W+$', y[0])) else 0 for y in result])/len(result)
+    )
 
 
 if __name__ == "__main__":
     corpus = 'wsj/wsj.text.train'
     pos = 'wsj/wsj.pos.train'
     bigram, p_w_given_g, tags, vocabulary = train(corpus, pos)
+    
+    score(bigram, p_w_given_g, tags, vocabulary)
 
-    tag(
-        "I need a flight from a",
-        bigram, p_w_given_g, tags, vocabulary
-    )
+    # tag(
+    #     "Pierre Vinken , 61 years old , will join the board as a nonexecutive director Nov. 29 . ",
+    #     bigram, p_w_given_g, tags, vocabulary
+    # )
