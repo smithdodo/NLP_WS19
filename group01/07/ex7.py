9ebc81ffa21c545277eec931e36f0692455a6847
Hengwen Zhang <h.w.zhangvic@gmail.com>
2019-12-03 15:31:17 +0100

    Bugfix

--- group01/07/ex7.py
+++ group01/07/ex7.py
@@ -5,8 +5,11 @@ import re
 import pandas as pd
 import numpy as np
 
+ADD_1_SMOOTHING = 'add_1_smoothing'
+LAPLACE_SMOOTHING= 'laplace_smoothing'
 
-def train(corpus, taggings):
+
+def train(corpus, taggings, smoothing=ADD_1_SMOOTHING):
     print('Training model...')
     bigram_pos = defaultdict(int)  # p(g_i|g_{i-1})
     p_w_given_g = defaultdict(int)  # p(w|g)
@@ -15,8 +18,6 @@ def train(corpus, taggings):
 
     f = read_files([corpus, taggings])
     seen = deque(['<S>'], maxlen=2)
-
-    cnt = 0
 
     while True:
         try:
@@ -36,10 +37,49 @@ def train(corpus, taggings):
             vocabulary.add(word)
             tags.add(tag)
 
-        cnt += 1
-        # if cnt >= 1000:
-        #     break
+    smoothing_func = smoothing_methods[smoothing]
+    bigram_pos, p_w_given_g = smoothing_func(bigram_pos, p_w_given_g, tags, vocabulary)
+
+    # convert pandas dataframe to dict
+    bigram_pos = {k: v['count'] for k, v in bigram_pos.to_dict('index').items()}
+    p_w_given_g = {k: v['count'] for k, v in p_w_given_g.to_dict('index').items()}
     
+    print('training finished')
+    return bigram_pos, p_w_given_g, list(tags), list(vocabulary)
+
+
+def laplace_smoothing(bigram_pos, p_w_given_g, tags, vocabulary):
+    for key in product(tags, repeat=2):
+        bigram_pos[key] += 0
+
+    for w in vocabulary:
+        for tag in tags:
+            p_w_given_g[(w, tag)] += 0
+
+    alpha = 0.4
+
+    bigram_pos = pd.DataFrame(
+        [[*k, v] for k, v in bigram_pos.items()], 
+        columns=['g_prev', 'g', 'count'],
+    )
+    bigram_pos.set_index(['g_prev', 'g'], inplace=True)
+    bigram_pos = (bigram_pos + alpha) / (bigram_pos.groupby('g_prev').sum() + alpha * len(tags))
+
+    p_w_given_g = pd.DataFrame(
+        [[*k, v] for k, v in p_w_given_g.items()],
+        columns=['w', 'g', 'count'],
+    )
+    p_w_given_g.set_index(['w', 'g'], inplace=True)
+    p_w_given_g = (p_w_given_g + alpha) / (p_w_given_g.groupby('g').sum() + alpha * len(tags))
+
+    print('laplace smoothing with alpha:', alpha)
+    print(bigram_pos.head())
+    print(p_w_given_g.head())
+
+    return bigram_pos, p_w_given_g
+
+
+def add_1_smoothing(bigram_pos, p_w_given_g, tags, vocabulary):
     # add-1 smoothing for p(g_i|g_{i-1})
     for key in product(tags, repeat=2):
         bigram_pos[key] += 1
@@ -48,7 +88,6 @@ def train(corpus, taggings):
     for w in vocabulary:
         for tag in tags:
             p_w_given_g[(w, tag)] += 1
-
 
     bigram_pos = pd.DataFrame(
         [[*k, v] for k, v in bigram_pos.items()], 
@@ -64,10 +103,16 @@ def train(corpus, taggings):
     p_w_given_g.set_index(['w', 'g'], inplace=True)
     p_w_given_g = p_w_given_g / p_w_given_g.groupby('g').sum()
 
+    print('add-1-smoothing:')
+    print(bigram_pos.head())
+    print(p_w_given_g.head())
+    return bigram_pos, p_w_given_g
 
-    bigram_pos = {k: v['count'] for k, v in bigram_pos.to_dict('index').items()}
-    p_w_given_g = {k: v['count'] for k, v in p_w_given_g.to_dict('index').items()}
-    return bigram_pos, p_w_given_g, list(tags), list(vocabulary)
+
+smoothing_methods = {
+    ADD_1_SMOOTHING: add_1_smoothing,
+    LAPLACE_SMOOTHING: laplace_smoothing
+}
 
 
 def _break_line(line):
@@ -131,7 +176,7 @@ def tag(sentance, bigram_pos, p_w_given_g, tags, vocabulary):
         else:
             result.append(tagging.pop(0))
 
-        print(f'{w:10s} -> {result[-1]}')
+        # print(f'{w:10s} -> {result[-1]}')
 
     return result
 
@@ -151,30 +196,28 @@ def read_files(paths):
         raise
 
 
-def score(bigram, p_w_given_g, tags, vocabulary):
-    corpus = 'wsj/wsj.text.test'
-    pos = 'wsj/wsj.pos.test'
+def score(bigram, p_w_given_g, tags, vocabulary, corpus, pos, max_sentances=-1):
+    print('testing...')
     f = read_files([corpus, pos])
 
     result = []
 
     cnt = 0
     while True:
-
         try:
             X, y = next(f)
             y = y.strip().split(' ')
             tagging = tag(X, bigram, p_w_given_g, tags, vocabulary)
             result += list(zip(y, tagging))
-        except Exception:
+        except:
             break
 
         cnt += 1
-        # if cnt > 10:
-        #     break
-    
+        if max_sentances > 0 and  cnt > max_sentances :
+            break
+
     print(
-        'Accuracy on test data:',
+        'Accuracy:',
          sum([1 if (y[0] == y[1] or re.match('^\W+$', y[0])) else 0 for y in result])/len(result)
     )
 
@@ -182,11 +225,16 @@ def score(bigram, p_w_given_g, tags, vocabulary):
 if __name__ == "__main__":
     corpus = 'wsj/wsj.text.train'
     pos = 'wsj/wsj.pos.train'
-    bigram, p_w_given_g, tags, vocabulary = train(corpus, pos)
+    bigram, p_w_given_g, tags, vocabulary = train(corpus, pos, smoothing=LAPLACE_SMOOTHING)
     
-    score(bigram, p_w_given_g, tags, vocabulary)
+    score(
+        bigram, p_w_given_g, tags, vocabulary,
+        'wsj/wsj.text.test',
+        'wsj/wsj.pos.test',
+        # max_sentances=1000
+    )
 
-    # tag(
-    #     "Pierre Vinken , 61 years old , will join the board as a nonexecutive director Nov. 29 . ",
-    #     bigram, p_w_given_g, tags, vocabulary
-    # )
+    # setance = "Pierre Vinken , 61 years old , will join the board as a nonexecutive director Nov. 29 . "
+    # tagging = tag(setance, bigram, p_w_given_g, tags, vocabulary)
+    # print(' '.join(tagging))
+    # print(setance)
